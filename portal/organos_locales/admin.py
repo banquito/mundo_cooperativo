77b7f32f4ca5ac1de4bbc7435e3238edb7ce781c
tiko2015 <tiko2015@gmail.com>
2014-09-22 14:06:27 -0300

    modelo y fixture para organos locales

--- portal/organos_locales/admin.py
+++ portal/organos_locales/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from organos_locales.models import Organization
+
+admin.site.register(Organization)
--- portal/organos_locales/models.py
+++ portal/organos_locales/models.py
@@ -1,20 +1,15 @@
 from django.db import models
 
-# Create your models here.
-class Province(models.Model):
-    name = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
 class Organization(models.Model):
-    province = models.ForeignKey(Province)
-    name = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
     siglas = models.CharField(max_length=200)
     domicilio = models.CharField(max_length=200)
-    codigo_postal = models.IntegerField()
+    codigo_postal = models.CharField(max_length=200)
     ciudad = models.CharField(max_length=200)
+    provincia = models.CharField(max_length=200)
     prefijo = models.IntegerField()
-    telefonos = models.IntegerField()
-    fax = models.IntegerField()
-    correo_electronico = models.CharField(max_length=200) 
+    telefonos = models.CharField(max_length=200)
+    fax = models.CharField(max_length=200, null=True)
+    correo_electronico = models.CharField(max_length=200, null=True) 
     director = models.CharField(max_length=200)
-    director2 = models.CharField(max_length=200)
+    director2 = models.CharField(max_length=200, null=True)
